c88a2901b2a637e7280419654ad9ebb26a3fce2d
rikodwi <riko.aa.aa.aa.aa@gmail.com>
2021-11-28 17:40:56 +0700

    perintah tugas 5

---
+++ operator.php
@@ -0,0 +1,18 @@
+<?php
+// operator aritmatika
+$x = 15;
+$y = 25;
+
+$tambah = $x + $y;
+echo "hasil penjumlahan = $tambah";
+echo "<br>";
+
+$kurang = $x - $y;
+echo "hasil pengurangan = $kurang";
+echo "<br>";
+
+$kali = $x * $y;
+echo "hasil perkalian = $kali";
+echo "<br>";
+
+?>
