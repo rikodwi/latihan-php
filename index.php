992aeb6600c3beae78f29493512fdbbbe3cf48ba
rikodwi <riko.aa.aa.aa.aa@gmail.com>
2021-11-28 14:34:06 +0700

    perintah tugas 1

---
+++ index.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>latihan php</title>
+</head>
+<body>
+    <h1>
+        selamat datang <?php echo "nama saya riko dwi" ?>
+    </h1>
+</body>
+</html>
